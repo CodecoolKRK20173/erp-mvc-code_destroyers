efea993f3eb9ceacc3bcaeb0ed41691b45850d9a
KaristonVeluraz <kg97@interia.pl>
2019-08-20 12:11:34 +0200

    Add firts variables to storecontroler

--- controller/store_controller.py
+++ controller/store_controller.py
@@ -15,3 +15,11 @@ def run():
     """
 
     # your code
+    file_name = "game_test.csv"
+
+    title = "Store menu"
+    list_options = ("(1) Add", "(2) Remove", "(3) Update")
+    exit_message = "(0) Back do main menu"
+
+
+    terminal_view.print_menu(title,list_options,exit_message)
